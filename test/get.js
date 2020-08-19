dd51e26abf4a7a8dc899246aaf0356a919c438bf
uikoo9 <uikoo9@qq.com>
2020-08-19 23:36:01 +0800

    get ok

--- test/get.js
+++ test/get.js
@@ -3,9 +3,13 @@
 var q = require('../lib/q-ajax');
 
 var test = async function(){
-    var url = 'http://icanhazip.com/';
-    var res = await q.get(url);
-    console.log(res);
+    try{
+        var url = 'http://icanhazip.com/';
+        var res = await q.get(url);
+        console.log(res);
+    }catch(e){
+        console.log(e);
+    }
 };
 
 test();
